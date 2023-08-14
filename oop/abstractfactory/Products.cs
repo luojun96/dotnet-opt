d1805ded8a428717491d5fe54daaf5d01145733e
Jun Luo <luojun96@live.cn>
2023-08-14 21:14:37 +0800

    reset project.

---
+++ oop/abstractfactory/Products.cs
@@ -0,0 +1,59 @@
+﻿namespace abstractfactory;
+
+interface IProductA {
+    public void DoSomething();
+}
+
+interface IProductB {
+    public void DoSomething();
+}
+
+class ProductA1 : IProductA {
+    public void DoSomething() {
+        Console.WriteLine("ProductA1");
+    }
+}
+
+class ProductA2 : IProductA {
+    public void DoSomething() {
+        Console.WriteLine("ProductA2");
+    }
+} 
+
+class ProductB1 : IProductB {
+    public void DoSomething() {
+        Console.WriteLine("ProductB1");
+    }
+}
+
+class ProductB2 : IProductB {
+    public void DoSomething() {
+        Console.WriteLine("ProductB2");
+    }
+}
+
+interface IAbstractFactory {
+    IProductA CreateProductA();
+    IProductB CreateProductB();
+}
+
+class ConcreteFactory1 : IAbstractFactory {
+    public IProductA CreateProductA() {
+        return new ProductA1();
+    }
+
+    public IProductB CreateProductB() {
+        return new ProductB1();
+    }
+}
+
+class ConcreteFactory2 : IAbstractFactory {
+    public IProductA CreateProductA() {
+        return new ProductA2();
+    }
+
+    public IProductB CreateProductB() {
+        return new ProductB2();
+    }
+}
+    
---
+++ oop/abstractfactory/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using abstractfactory;
+
+Console.WriteLine("Hello, World!");
+
+IAbstractFactory factory1 = new ConcreteFactory1();
+
+IProductA productA1 = factory1.CreateProductA();
+IProductB productB1 = factory1.CreateProductB();
+
+productA1.DoSomething();
+productB1.DoSomething();
